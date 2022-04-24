88cff14014c7668538bb756c33ddd4e95be7fdd6
Aleja2020 <jorge.alejandra.uenla@gmail.com>
2022-04-24 12:23:10 -0400

    Initial commit

---
+++ tic-tac-script.js
@@ -0,0 +1,13 @@
+document.addEventListener("load", myFunction);
+
+myFunction(); {
+    var cells = document.getElementsByClassName("cell");
+    var caracter = document.getElementById("svg-whatever").style.display = "block";
+
+    cells.forEach(cell => {
+        cell.addEventListener("click", {
+            
+
+        });
+    });
+}
